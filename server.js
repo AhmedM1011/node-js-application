6d7b513ad67c1bc45ab6c8aca8f75af2e9446b10
AhmedM1011 <ahmedmanegar2608@gmail.com>
2024-06-15 11:27:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+//DOT ENV
+dotenv.config();
+
+//MONDODB Connection
+connectDB();
+
+// REST OBJECT
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//ROUTES
+// app.get("", (req, res) => {
+// 	res.status(200).json({
+// 		success: true,
+// 		message: "Welcome to full stack app",
+// 	});
+// });
+app.use("/api/v1/auth", require("./routes/userRoutes"));
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//Listen
+app.listen(PORT, () => {
+	console.log(`Server Running ${PORT}`.bgGreen.white);
+});
