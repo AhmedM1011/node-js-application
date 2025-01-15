135e115d73f94021edde3975271dc7a2764dfdba
Ahmed Manegar <ahmedmanegar@Ahmeds-MacBook-Air.local>
2025-01-15 08:03:30 +0400

    update mongo promise

--- config/db.js
+++ config/db.js
@@ -4,14 +4,23 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.DATABASE_URL).then(() => {
+        console.log('mongodb connected');
+      
+      }).catch((err) => {
+        console.log(err);
+      
+      })
+    // try {
+    //     mongoose.connect(process.env.MONGO_URL);
 
-        console.log(`Connected to DATABASE ${conn.connection.host}`.bgCyan.white);
-    } catch (error) {
-        console.error(`Error in connection DB: ${error.message}`.bgRed.white);
-        process.exit(1); // Exit process with failuree
-    }
+    //     console.log(`Connected to DATABASE ${conn.connection.host}`.bgCyan.white);
+    // } catch (error) {
+    //     console.error(`Error in connection DB: ${error.message}`.bgRed.white);
+    //     process.exit(1); // Exit process with failuree
+    // }
 };
+
+
 
 module.exports = connectDB;
